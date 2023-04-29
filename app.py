760ebe9bf6ef64b81924a7032cf9314db75eca86
Cyriac <cyriac.thibaudeau@ens.uvsq.fr>
2023-04-29 16:05:16 +0200

    up first

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from views import views
+
+
+app = Flask(__name__)
+#Lien avec les blueprints créé
+app.register_blueprint(views, url_prefix="/views")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000) #le port de base est 5000, peut être changé
---
+++ views.py
@@ -0,0 +1,29 @@
+#Possibilité de mettre nos routes dans le app.py après initialisation de l'app, mais ici on les orgnasiera sous forme de Blueprint
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+
+#Initialisation du Blueprint
+views = Blueprint(__name__, "views")
+
+@views.route("/") 
+def home():
+    #Retourner un code html
+    return render_template('index.html', name="Cyriac", age= 20) #on peut ajouter autant de variables que l'on veut et y acceder dans index.html avec {{variable}}
+
+@views.route("/profile")
+def profile():
+    #request est là pour une query à la place de mettre comme paramètre de la fonction
+    return render_template('profile.html')
+
+@views.route("/json")
+def get_json():
+    #Ici, on jsonify un dico python
+    return jsonify({'name' : 'cyriac', 'coolness' : 40})
+
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+@views.route("/go-to-home")
+def go_to_home():
+    return redirect(url_for("views.home"))
